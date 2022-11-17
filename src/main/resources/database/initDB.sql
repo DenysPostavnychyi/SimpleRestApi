aae7cfd842d7f5ac5a8c4c1d3e5341893ca735fe
DenisPostavnychyi <denozaurus03@ukr.net>
2022-11-17 19:26:48 +0200

    Fix pom, initDb and populateDB

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,13 +1,13 @@
 CREATE TABLE columns
 (
-    sequenceNumber SERIAL PRIMARY KEY NOT NULL,
-    name           TEXT               NOT NULL
+    id   SERIAL PRIMARY KEY NOT NULL,
+    name TEXT               NOT NULL
 );
 
 CREATE TABLE tasks
 (
     id             SERIAL,
-    columns_id     SERIAL       NOT NULL REFERENCES columns (sequenceNumber) ON DELETE CASCADE,
+    columns_id     SERIAL       NOT NULL REFERENCES columns (id) ON DELETE CASCADE,
     name           TEXT         NOT NULL,
     description    TEXT         NOT NULL,
     dateOfCreation varchar(256) NOT NULL
--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,6 +1,6 @@
-INSERT INTO columns (sequenceNumber, name) VALUES
-                                               (1, "One"),
-                                               (2, "Two"),
-                                               (3, "Tree"),
-                                               (4, "Four"),
-                                               (5, "Five")
+INSERT INTO columns (id, name) VALUES
+                                               (1, 'One'),
+                                               (2, 'Two'),
+                                               (3, 'Tree'),
+                                               (4, 'Four'),
+                                               (5, 'Five')
