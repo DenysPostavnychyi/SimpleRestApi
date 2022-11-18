04ae804db1f03248144340456e645b4f9ffe922c
DenisPostavnychyi <denozaurus03@ukr.net>
2022-11-18 09:52:08 +0200

    Fix Column entity class

--- src/main/java/com/example/model/Column.java
+++ src/main/java/com/example/model/Column.java
@@ -3,25 +3,23 @@ package com.example.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
+@Entity
 @Getter
 @Setter
 @Table(name = "columns")
 public class Column {
-  private static int count = 0;
-
-  @javax.persistence.Column(name = "sequenceNumber")
-  private int sequenceNumber;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @javax.persistence.Column(name = "id")
+  private Long id;
 
   @javax.persistence.Column(name = "name")
   private String name;
-
-  public Column(String name) {
-    this.name = name;
-    sequenceNumber = ++count;
-  }
 }
