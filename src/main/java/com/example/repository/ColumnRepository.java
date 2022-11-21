de63958508f9134fa667c51837beb649caa72be7
DenisPostavnychyi <denozaurus03@ukr.net>
2022-11-21 18:14:39 +0200

    Add methods to repositories

--- src/main/java/com/example/repository/ColumnRepository.java
+++ src/main/java/com/example/repository/ColumnRepository.java
@@ -2,9 +2,24 @@ package com.example.repository;
 
 import com.example.model.Column;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface ColumnRepository extends JpaRepository<Column, Long> {
+  @Modifying
+  @Query("UPDATE Column c SET c.name = ?1 WHERE c.id = ?2")
+  @Transactional
+  void changeName(String name, Long id);
 
+  @Modifying
+  @Query("UPDATE Column c SET c.sequenceNumber = ?1 WHERE c.id = ?2")
+  @Transactional
+  void changeSequenceNumber(Integer sequenceNumber, Long id);
+
+  List<Column> findAllByOrderBySequenceNumber();
 }
--- src/main/java/com/example/repository/TaskRepository.java
+++ src/main/java/com/example/repository/TaskRepository.java
@@ -2,8 +2,20 @@ package com.example.repository;
 
 import com.example.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+  @Modifying
+  @Query("UPDATE Task t SET t.sequenceNumber = ?1 WHERE t.id = ?2")
+  @Transactional
+  void changeSequenceNumber(Integer sequenceNumber, Long id);
+
+  List<Task> findAllByOrderBySequenceNumber();
 }
